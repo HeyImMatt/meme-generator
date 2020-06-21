946caba02ea3809f2a7b6566eacce69d72249b44
heyimmatt <mrmcfarland@gmail.com>
2020-06-21 10:37:00 -0700

    Create meme div and apply background image with supplied url

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const form = document.querySelector('#create-meme-form');
 const imageUrlField = document.querySelector('#image-link');
 const topTextField = document.querySelector('#top-text');
 const bottomTextField = document.querySelector('#bottom-text');
+const generatedMemesDiv = document.querySelector('#generated-memes')
 
 form.onsubmit = function(event) {
   event.preventDefault();
   const imageUrl = imageUrlField.value;
   const topText = topTextField.value;
   const bottomText = bottomTextField.value;
-  console.log(imageUrl, topText, bottomText)
+
+  createSpicyMeme(imageUrl);
+
   imageUrlField.value = '';
   topTextField.value = '';
   bottomTextField.value = '';
+}
+
+function createSpicyMeme(url, topText, bottomText) {
+  const meme = document.createElement('div');
+  meme.className = 'meme';
+  meme.style.backgroundImage = `url(${url})`
+  generatedMemesDiv.append(meme);
 }
