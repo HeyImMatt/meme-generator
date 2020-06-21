c9ee0711c3bdcc36142aeaa37f4a368e5af9e341
heyimmatt <mrmcfarland@gmail.com>
2020-06-21 11:02:16 -0700

    Add top text to meme and style

--- app.js
+++ app.js
@@ -2,24 +2,30 @@ const form = document.querySelector('#create-meme-form');
 const imageUrlField = document.querySelector('#image-link');
 const topTextField = document.querySelector('#top-text');
 const bottomTextField = document.querySelector('#bottom-text');
-const generatedMemesDiv = document.querySelector('#generated-memes')
+const generatedMemesDiv = document.querySelector('#generated-memes');
 
-form.onsubmit = function(event) {
+form.onsubmit = function (event) {
   event.preventDefault();
   const imageUrl = imageUrlField.value;
   const topText = topTextField.value;
   const bottomText = bottomTextField.value;
 
-  createSpicyMeme(imageUrl);
+  createSpicyMeme(imageUrl, topText, bottomText);
 
   imageUrlField.value = '';
   topTextField.value = '';
   bottomTextField.value = '';
-}
+};
 
 function createSpicyMeme(url, topText, bottomText) {
   const meme = document.createElement('div');
+  const topTextArea = document.createElement('span');
+
   meme.className = 'meme';
-  meme.style.backgroundImage = `url(${url})`
+  meme.style.backgroundImage = `url(${url})`;
+  topTextArea.className = 'top-text';
+  topTextArea.innerText = topText;
+
+  meme.append(topTextArea)
   generatedMemesDiv.append(meme);
 }
